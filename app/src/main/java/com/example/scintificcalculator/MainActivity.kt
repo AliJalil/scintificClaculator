e065cee2b079543c85a3b79814c170711ddc755a
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-05-01 23:40:03 +0300

    1st commit - 01

--- app/src/main/java/com/example/scintificcalculator/MainActivity.kt
+++ app/src/main/java/com/example/scintificcalculator/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.scintificcalculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 import androidx.databinding.DataBindingUtil
@@ -12,6 +14,19 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var calTxt : TextView
     lateinit var resultTxt : TextView
+
+    lateinit var btn0 : Button
+    lateinit var btn1 : Button
+    lateinit var btn2 : Button
+    lateinit var btn3 : Button
+    lateinit var btn4 : Button
+    lateinit var btn5 : Button
+    lateinit var btn6 : Button
+    lateinit var btn7 : Button
+    lateinit var btn8 : Button
+    lateinit var btn9 : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +38,20 @@ class MainActivity : AppCompatActivity() {
         calTxt = binding.calTxt
         resultTxt = binding.resultTxt
 
+        btn0 = binding.btn0
+        btn1 = binding.btn1
+        btn2 = binding.btn2
+        btn3 = binding.btn3
+        btn4 = binding.btn4
+        btn5 = binding.btn5
+        btn6 = binding.btn6
+        btn7 = binding.btn7
+        btn8 = binding.btn8
+        btn9 = binding.btn9
+
+        btn0.setOnClickListener {
+            calTxt.text = "${calTxt.text} 0"
+        }
 
     }
 }
